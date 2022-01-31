37752417c58f43f75403316dc52c6d87aa78c90f
Ho Xuan Anh <frt.hxanh>
2022-01-31 14:34:36 +0700

    update footer

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -1,3 +1,55 @@
 export default function Footer() {
-  return <div>Đây là Footer</div>;
+  return (
+    <footer className="container-fluid mt-5 ">
+      <div className="container">
+        <div className="row footer">
+          <div className="col-6 footer_cool">
+            <h2>Coolidge Corner Theatre Foundation</h2>
+            <p className="footer_cool__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              magnam id esse laboriosam deserunt tempore impedit nostrum
+              consequuntur facere sunt perferendis et ea, illum placeat nam
+              blanditiis ipsum veniam expedita.
+            </p>
+          </div>
+          <div className="col-2">
+            <h3>CONTACT US </h3>
+            <div className="widget">
+              <ul>
+                <li>290 Harvard Street, Brookline MA 02446</li>
+                <li>P / 6176-938-2222</li>
+                <li>Office 9389-393-3333</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-2">
+            <h3>MORE LINKS</h3>
+            <div className="widget">
+              <ul>
+                <li>Showtimes</li>
+                <li>Programs</li>
+                <li>About Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-2">
+            <h3>FOLLOW US</h3>
+            <div className="widget">
+              <ul className="icons">
+                <li className="icon">
+                  <i className="fab fa-facebook"></i>
+                </li>
+                <li className="icon">
+                  <i className="fab fa-instagram-square"></i>
+                </li>
+                <li className="icon">
+                  <i className="fab fa-twitter-square"></i>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
