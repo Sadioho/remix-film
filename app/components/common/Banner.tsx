655085a642ea0f0fd25396c67a2bf5a5d7a87526
HoaCong <leconghoa11a1@gmail.com>
2022-02-05 14:24:15 +0700

    update

--- app/components/common/Banner.tsx
+++ app/components/common/Banner.tsx
@@ -4,26 +4,23 @@ import { Film } from "~/api/films";
 
 export default function Banner(props: any) {
   const { data } = props;
+  console.log("Banner ~ data", data);
   const films = data.recommendContentVOList;
   const refDrag = useRef<HTMLDivElement>(null);
   const refSpin = useRef<HTMLDivElement>(null);
   const refGround = useRef<HTMLDivElement>(null);
+  let radius = 480; // vòng tròn ảnh
+  const autoRotate = true; // xoay
+  const rotateSpeed = -60; // unit: seconds/360 degrees
+  let imgWidth = 400; // width
+  let imgHeight = 250; // height
+
   useEffect(() => {
-    let radius = 480; // vòng tròn ảnh
-    const autoRotate = true; // xoay
-    const rotateSpeed = -60; // unit: seconds/360 degrees
-    let imgWidth = 400; // width
-    let imgHeight = 250; // height
-    let desX = 0,
-      desY = 0,
-      tX = 0,
-      tY = 10;
     const odrag = refDrag.current;
     const circleSpin = refSpin.current;
+    const ground = refGround.current;
     const images: any = circleSpin?.getElementsByClassName("item-carousel");
     const elements = [...images]; // arrays
-    const ground = refGround.current;
-
     const init = (delayTime: any) => {
       for (let i = 0; i < elements.length; i++) {
         elements[i].style.transform =
@@ -52,20 +49,6 @@ export default function Banner(props: any) {
       ground.style.height = radius * 3 + "px";
     }
 
-    const applyTranform = (obj: any) => {
-      // Constrain the angle of camera (between 0 and 180)
-      if (tY > 180) tY = 180;
-      if (tY < 0) tY = 0;
-
-      // Apply the angle
-      obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
-    };
-
-    const playSpin = (yes: any) => {
-      if (circleSpin)
-        circleSpin.style.animationPlayState = yes ? "running" : "paused";
-    };
-
     // auto spin
     if (autoRotate) {
       const animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
@@ -75,60 +58,78 @@ export default function Banner(props: any) {
         )}s infinite linear`;
     }
 
-    // setup events
-    document.onpointerdown = (e) => {
-      e = e || window.event;
-      let sX = e.clientX,
-        sY = e.clientY;
+    // // setup events
+    // let desX = 0,
+    // desY = 0,
+    // tX = 0,
+    // tY = 10;
+    // const applyTranform = (obj: any) => {
+    //   // Constrain the angle of camera (between 0 and 180)
+    //   if (tY > 180) tY = 180;
+    //   if (tY < 0) tY = 0;
 
-      document.onpointermove = (e) => {
-        e = e || window.event;
-        const nX = e.clientX,
-          nY = e.clientY;
-        desX = nX - sX;
-        desY = nY - sY;
-        tX += desX * 0.1;
-        tY += desY * 0.1;
-        applyTranform(odrag);
-        sX = nX;
-        sY = nY;
-      };
+    //   // Apply the angle
+    //   obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
+    // };
 
-      document.onpointerup = (e) => {
-        setInterval(() => {
-          desX *= 0.95;
-          desY *= 0.95;
-          tX += desX * 0.1;
-          tY += desY * 0.1;
-          applyTranform(odrag);
-          playSpin(false);
-          if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
-            playSpin(true);
-          }
-        }, 17);
-        document.onpointermove = document.onpointerup = null;
-      };
-      return false;
-    };
+    // const playSpin = (yes: any) => {
+    //   if (circleSpin)
+    //     circleSpin.style.animationPlayState = yes ? "running" : "paused";
+    // };
+    // document.onpointerdown = (e) => {
+    //   e = e || window.event;
+    //   let sX = e.clientX,
+    //     sY = e.clientY;
+    //   // thả chuột
+    //   document.onpointermove = (e) => {
+    //     e = e || window.event;
+    //     const nX = e.clientX,
+    //       nY = e.clientY;
+    //     desX = nX - sX;
+    //     desY = nY - sY;
+    //     tX += desX * 0.1;
+    //     tY += desY * 0.1;
+    //     applyTranform(odrag);
+    //     sX = nX;
+    //     sY = nY;
+    //   };
+    //   // nhấn chuột
+    //   document.onpointerup = (e) => {
+    //     setInterval(() => {
+    //       desX *= 0.95;
+    //       desY *= 0.95;
+    //       tX += desX * 0.1;
+    //       tY += desY * 0.1;
+    //       applyTranform(odrag);
+    //       playSpin(false);
+    //       if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+    //         playSpin(true);
+    //       }
+    //     }, 17);
+    //     document.onpointermove = document.onpointerup = null;
+    //   };
+    //   return false;
+    // };
 
-    document.onwheel = (e) => {
-      e = e || window.event;
-      const d = e.deltaY / 20 || -e.detail;
-      radius < 250
-        ? (radius = 250)
-        : radius > 550
-        ? (radius = 550)
-        : (radius += d);
-      imgWidth += d / 2;
+    // // xoay lăn chuột
+    // document.onwheel = (e) => {
+    //   e = e || window.event;
+    //   const d = e.deltaY / 20 || -e.detail;
+    //   radius < 250
+    //     ? (radius = 250)
+    //     : radius > 550
+    //     ? (radius = 550)
+    //     : (radius += d);
+    //   imgWidth += d / 2;
 
-      if (circleSpin) {
-        if (imgWidth < 250) imgWidth = 250;
-        if (imgWidth > 450) imgWidth = 450;
-        circleSpin.style.width = imgWidth + "px";
-      }
+    //   if (circleSpin) {
+    //     if (imgWidth < 250) imgWidth = 250;
+    //     if (imgWidth > 450) imgWidth = 450;
+    //     circleSpin.style.width = imgWidth + "px";
+    //   }
 
-      init(1);
-    };
+    //   init(1);
+    // };
   }, []);
 
   return (
